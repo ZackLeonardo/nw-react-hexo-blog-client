54498ab3757b0eabbf418cfeccf32f9fdaa6c0c3
ZackLeonardo <maiz9088@icloud.com>
2015-11-15 23:07:16 +0800

    change folder.

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,19 @@ var babel = require("gulp-babel");
 gulp.task('default',['one', 'two', 'three']);
 
 gulp.task("one", function () {
-  return gulp.src("src/components/app.js")
+  return gulp.src("src/components/md/app.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist/components"));
+    .pipe(gulp.dest("dist/components/md"));
 });
 
 gulp.task("two", function () {
-  return gulp.src("src/components/MDEditor.js")
+  return gulp.src("src/components/md/MDEditor.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist/components"));
+    .pipe(gulp.dest("dist/components/md"));
 });
 
 gulp.task("three", function () {
-  return gulp.src("src/components/format.js")
+  return gulp.src("src/components/md/format.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist/components"));
+    .pipe(gulp.dest("dist/components/md"));
 });
