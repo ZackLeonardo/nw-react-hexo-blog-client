bc1cccc9d49f215f388c493b34dd9e0b6bf2372c
ZackLeonardo <maiz9088@icloud.com>
2015-11-17 14:12:25 +0800

    add menu and click events.

--- src/components/nwGui/nwGui.js
+++ src/components/nwGui/nwGui.js
@@ -1,26 +1,42 @@
+// Load native UI library
 var gui = require('nw.gui');
 
-// // Standard way of creating elements
-// // var element = new gui.ElementName(option);
-//
-// // Example of creating a menu
-// var menu = new gui.Menu({ title: 'Menu Title' });
-//
-// menu.title = 'New Title';
-// console.log('Menu title is', menu.title);
-//
-// // menu.append(new gui.MenuItem({ label: 'Im an item' }));
-// // menu.removeAt(0);
-//
-// menuitem.on('click', function() {
-//   console.log('Item is clicked');
-// });
-
 win = gui.Window.get();
+
+// Create menu
 var nativeMenuBar = new gui.Menu({ type: "menubar" });
+
+// create MacBuiltin
+nativeMenuBar.createMacBuiltin("nw-blog",{
+  hideEdit: false,
+  hideWindow: false
+});
+
+// Create sub-menu
+var menuItems = new gui.Menu();
+menuItems.append(new gui.MenuItem({ label: '配置node' }));
+menuItems.append(new gui.MenuItem({ label: '配置hexo' }));
+
+// Append MenuItem as a Submenu
+var menu_conf = new gui.MenuItem({
+  label: '配置环境',
+  submenu: menuItems // menu elements from menuItems object
+})
+nativeMenuBar.append(menu_conf);
+
+menuItems.items[0].click = function() {
+  // win.close();
+  alert("配置node");
+};
+
+menuItems.items[1].click = function() {
+  // win.close();
+  alert("配置hexo");
+};
+
+win.menu = nativeMenuBar;
+
 try {
-  nativeMenuBar.createMacBuiltin("nw-blog");
-  win.menu = nativeMenuBar;
 } catch (ex) {
   console.log(ex.message);
 }
