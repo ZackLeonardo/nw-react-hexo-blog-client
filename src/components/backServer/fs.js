7c47c4c3b2d715f5f39ceb04e0f3dc79df7d6974
ZackLeonardo <maiz9088@icloud.com>
2015-11-21 23:34:10 +0800

    rm and mk dir

--- src/components/backServer/fs.js
+++ src/components/backServer/fs.js
@@ -1,1 +1,34 @@
 'use strict';
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+//清空文件夹并删除之
+var deleteFolderRecursive = function(path) {
+  if (path != ''){
+    var files = [];
+    if( fs.existsSync(path) ) {
+        files = fs.readdirSync(path);
+        files.forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+  }
+};
+
+var deleteFolderCMD = function (path) {
+  exec('rm -rf ' + path ,function(err,out) {
+    console.log(out); err && console.log(err);
+  });
+}
+
+//创建文件夹
+var mkdir = function (path) {
+  fs.mkdirSync(path);
+}
