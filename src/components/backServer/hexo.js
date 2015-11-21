0d8ff6b6f3963ec810f9d93874d470ee308fb10e
ZackLeonardo <maiz9088@icloud.com>
2015-11-22 00:33:51 +0800

    hexo init done!

---
+++ src/components/backServer/hexo.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// var hexo = require('hexo');
+var exec = require('child_process').exec;
+
+var hexoInit = function (path) {
+  exec('hexo init ' + path ,function (error, stdout, stderr) {
+    // console.log('stdout: ' + stdout);
+    // console.log('stderr: ' + stderr);
+    document.getElementById("hexo_logs").innerHTML = stdout;
+    hexoNpmInstall(path);
+    if (error !== null) {
+      // console.log('exec error: ' + error);
+      document.getElementById("hexo_logs").innerHTML = error;
+    }
+});
+
+}
+
+var hexoNpmInstall = function (path) {
+  console.log(path);
+  exec('npm install ' + path,{cwd: path} ,function (error, stdout, stderr) {
+    document.getElementById("npm_logs").innerHTML = stdout;
+    if (error !== null) {
+      // console.log('exec error: ' + error);
+      document.getElementById("npm_logs").innerHTML = error;
+    }
+  });
+}
