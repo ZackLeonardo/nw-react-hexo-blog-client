4c13934dc793db6e83042e097ebcd2e1f079333b
ZackLeonardo <maiz9088@icloud.com>
2015-12-22 23:21:49 +0800

    add hexo config html

--- js/hexoConfig/hexoConfig.js
+++ js/hexoConfig/hexoConfig.js
@@ -1,4 +1,6 @@
-var data = {	title:	'Cleaning Supplies',
-           supplies:	['mop', 'broom', 'duster']	};
+
 var html = new EJS({url: 'hexoConfig.ejs'}).render(data);
 $("#container").html(html);//
+
+var data = {	title:	'Cleaning Supplies',
+           supplies:	['mop', 'broom', 'duster']	};
